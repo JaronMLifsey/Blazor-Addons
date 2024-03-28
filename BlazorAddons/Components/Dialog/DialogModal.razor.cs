7f3701dcf0ba1fda54d0e030552ee01b7c7e6ec4
Jaron Lifsey <7410825+JaronMLifsey@users.noreply.github.com>
2024-03-27 22:28:04 -0700

    Mostly works.

--- BlazorAddons/Components/Dialog/DialogModal.razor.cs
+++ BlazorAddons/Components/Dialog/DialogModal.razor.cs
@@ -12,11 +12,50 @@ namespace BlazorAddons
         public EventCallback<bool> OpenChanged { get; set; }
 
         [Parameter]
+        public string? Class { get; set; }
+
+        [Parameter]
         public RenderFragment? ChildContent { get; set; }
 
         [Parameter]
         public bool Open { get; set; } = false;
         public bool? _Open;
+
+        /// <summary>
+        /// The origin point of the containing stacking context which <see cref="Transform"/> will be based from. 
+        /// (0.5, 0.5) is the center, (0, 0) is the top left and (1, 1) is the bottom right.
+        /// Defaults to (0.5, 1) which is the bottom center of the containing context.
+        /// </summary>
+        /// <remarks>Does nothing if <see cref="AsModal"/> is true."/></remarks>
+        [Parameter]
+        public (double X, double Y) Origin { get; set; } = (0.5, 1);
+        public (double X, double Y)? _Origin;
+
+        /// <summary>
+        /// The point on the dialog which will be placed on <see cref="Origin"/>.
+        /// (0, 0) is the top left, (0.5, 0.5) is the center, and (1, 1) is the bottom right.
+        /// Defaults to (0.5, 0) which is the top center of the dialog.
+        /// </summary>
+        /// <remarks>Does nothing if <see cref="AsModal"/> is true."/></remarks>
+        [Parameter]
+        public (double X, double Y) Transform { get; set; } = (0.5, 0);
+        public (double X, double Y)? _Transform;
+
+        /// <summary>
+        /// The pixel offset of the dialog from where <see cref="Transform"/> and <see cref="Origin"/> would put the dialog.
+        /// </summary>
+        /// <remarks>Does nothing if <see cref="AsModal"/> is true."/></remarks>
+        [Parameter]
+        public (double X, double Y) OffsetPixels { get; set; } = (0, 0);
+        public (double X, double Y)? _OffsetPixels;
+
+        /// <summary>
+        /// The minimum number of pixels between the edge of the screen and the dialog.
+        /// </summary>
+        /// <remarks>Does nothing if <see cref="AsModal"/> is true."/></remarks>
+        [Parameter]
+        public double ViewPortPadding { get; set; } = 20;
+        public double? _ViewPortPadding;
 
         /// <summary>
         /// Displays the Dialog as a modal which overlays the rest of the page.
@@ -55,14 +94,22 @@ namespace BlazorAddons
                     || CloseOnClickOutside != _CloseOnClickOutside
                     || AsModal != _AsModal 
                     || Open != _Open
+                    || Origin != _Origin
+                    || Transform != _Transform
+                    || OffsetPixels != _OffsetPixels
+                    || ViewPortPadding != _ViewPortPadding
                 ))
             {
                 _CloseOnClickEscape = CloseOnEscape;
                 _CloseOnClickOutside = CloseOnClickOutside;
                 _AsModal = AsModal;
                 _Open = Open;
+                _Origin = Origin;
+                _Transform = Transform;
+                _OffsetPixels = OffsetPixels;
+                _ViewPortPadding = ViewPortPadding;
 
-                await DialogJsObj.InvokeVoidAsync("configurDialog", CloseOnEscape, CloseOnClickOutside, AsModal, Open);
+                await DialogJsObj.InvokeVoidAsync("configurDialog", CloseOnEscape, CloseOnClickOutside, AsModal, Open, Origin.X, Origin.Y, Transform.X, Transform.Y, OffsetPixels.X, OffsetPixels.Y, ViewPortPadding);
             }
 
         }
--- BlazorAddons/Components/Dialog/DialogModal.razor.ts
+++ BlazorAddons/Components/Dialog/DialogModal.razor.ts
@@ -5,9 +5,23 @@ class DialogModal {
     private closeOnClickOutside: boolean = false;
     private closeOnEscape: boolean = false;
     private dotNetObject: any | null = null;
+    private resizeObserver: ResizeObserver;
+    private resizeObservedParent: Element | null = null;
 
     private clickOutsideHanlder: any;
     private escapePressedHanlder: any;
+    private resizeHanlder: any;
+
+    //The origin point within the nearest positioned ancestor of the dialog. (-1, -1) is mapped to the top left and (1, 1) to the bottom right.
+    private origin: { x: number, y: number } = { x: 0, y: 0 };
+    //The point within the dialog element which will be placed on the origin. (-1, -1) is mapped to the top left and (1, 1) to the bottom right.
+    private transform: { x: number, y: number } = { x: 0, y: 0 };
+    //An offset which is applied to the final position.
+    private offset: { x: number, y: number } = { x: 0, y: 0 };
+
+    private viewportPadding: number = 20;
+
+    private currentPosition: { x: number, y: number } = { x: 0, y: 0 };
 
     constructor(dialog: HTMLDialogElement, dotNetObject: any) {
         this.dialog = dialog;
@@ -15,23 +29,56 @@ class DialogModal {
 
         this.clickOutsideHanlder = this.handleClickOutside.bind(this);
         this.escapePressedHanlder = this.handleEscapePressed.bind(this);
+        this.resizeHanlder = this.positionDialog.bind(this);
+
+        this.resizeObserver = new ResizeObserver((element) => {
+            this.positionDialog();
+        });
     }
 
     private open(asModal: boolean) {
         document.addEventListener("click", this.clickOutsideHanlder);
         document.addEventListener("keyup", this.escapePressedHanlder);
 
+        this.currentPosition = { x: 0, y: 0 };
+
+        if (asModal) {
+            this.dialog.style.marginRight = "auto";
+            this.dialog.style.marginLeft = "auto";
+            this.dialog.style.top = null;
+            this.dialog.style.left = "0px";
+            this.dialog.style.right = "0px";
+        }
+        else {
+            this.dialog.style.marginRight = null;
+            this.dialog.style.marginLeft = null;
+            this.dialog.style.top = "0px";
+            this.dialog.style.left = "0px";
+            this.dialog.style.right = null;
+        }
+
         if (asModal) {
             this.dialog.showModal();
+            this.resizeObservedParent = null;
         }
         else {
             this.dialog.show();
+            window.addEventListener("resize", this.resizeHanlder);
+            this.resizeObservedParent = this.dialog.parentElement;
+            this.resizeObserver.observe(this.resizeObservedParent);
+            this.positionDialog();
         }
     }
 
     private close(notify: boolean) {
         document.removeEventListener("click", this.clickOutsideHanlder);
         document.removeEventListener("keyup", this.escapePressedHanlder);
+        window.removeEventListener("resize", this.resizeHanlder);
+
+        if (this.resizeObservedParent != null) {
+            this.resizeObserver.unobserve(this.resizeObservedParent);
+            this.resizeObservedParent = null;
+        }
 
         this.dialog.close();
         if (notify) {
@@ -39,7 +86,7 @@ class DialogModal {
         }
     }
 
-    public handleEscapePressed(event: KeyboardEvent) {
+    private handleEscapePressed(event: KeyboardEvent) {
         if (this.closeOnEscape && event.key !== "Escape") {
             return;
         }
@@ -48,7 +95,7 @@ class DialogModal {
         event.preventDefault();
     }
 
-    public handleClickOutside(event: PointerEvent) {
+    private handleClickOutside(event: PointerEvent) {
         if (!this.closeOnClickOutside) {
             return;
         }
@@ -64,10 +111,60 @@ class DialogModal {
         }
     }
 
-    public async configurDialog(closeOnEscape: boolean, closeOnClickOutside: boolean, asModal: boolean, open: boolean) {
+    private positionDialog() {
+        //let container = this.dialog.offsetParent as HTMLElement;
+        let container = this.dialog.parentElement as HTMLElement;
+
+        if (container == null) {  
+            return;
+        }
+
+        let containerBounds = container.getBoundingClientRect();
+        let dialogBounds = this.dialog.getBoundingClientRect();
+
+        //Get the reference points on the container and dialog element
+        let originPoint = { x: containerBounds.left + this.origin.x * containerBounds.width, y: containerBounds.top + this.origin.y * containerBounds.height };
+        let transformPoint = { x: dialogBounds.left + this.transform.x * dialogBounds.width, y: dialogBounds.top + this.transform.y * dialogBounds.height };
+
+        //Get the offsets
+        let offset = { x: this.offset.x +  originPoint.x - transformPoint.x, y: this.offset.y + originPoint.y - transformPoint.y };
+
+        if (dialogBounds.left + offset.x - this.viewportPadding < 0) {
+            offset.x = this.viewportPadding - dialogBounds.left;
+        }
+        if (dialogBounds.right + offset.x + this.viewportPadding > window.innerWidth) {
+            offset.x = window.innerWidth - this.viewportPadding - dialogBounds.right;
+        }
+
+        offset.x += this.currentPosition.x;
+        offset.y += this.currentPosition.y;
+
+        this.currentPosition = offset;
+
+        this.dialog.style.left = `${offset.x}px`;
+        this.dialog.style.top = `${offset.y}px`;
+    }
+
+    public async configurDialog(
+        closeOnEscape: boolean,
+        closeOnClickOutside: boolean,
+        asModal: boolean,
+        open: boolean,
+        originX: number,
+        originY: number,
+        transformX: number,
+        transformY: number,
+        offsetX: number,
+        offsetY: number,
+        viewportPadding: number
+    ) {
         this.closeOnEscape = closeOnEscape;
         this.closeOnClickOutside = closeOnClickOutside;
-
+        this.transform = { x: transformX, y: transformY };
+        this.origin = { x: originX, y: originY };
+        this.offset = { x: offsetX, y: offsetY };
+        this.viewportPadding = viewportPadding;
+        this.dialog.style.maxWidth = `calc(100vw - ${this.viewportPadding * 2}px)`;
 
         if (this.dialog.open != open) {
             if (open) {
